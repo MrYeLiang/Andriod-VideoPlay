3ffdfd8c38a9ec2ba71d1ecff19232cf38802365
叶亮 <yeliang@tv365.net>
2018-12-23 21:04:15 +0800

    临时提交

---
+++ VideoPlay/practice_01/src/main/cpp/GLVideoView.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 叶亮 on 2018/12/22.
+//
+
+#include "GLVideoView.h"
---
+++ VideoPlay/practice_01/src/main/cpp/XTexture.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 叶亮 on 2018/12/22.
+//
+#include "XTexture.h"
+#include "XLog.h"
+
+class CXTexture:public XTexture
+{
+public:
+    XShader sh;
+    XTextureType  type;
+    std:mux
+};
