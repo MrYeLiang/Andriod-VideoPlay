34f8d27844d20e383a2fc729f1562a4cfcc6eae7
叶亮 <yeliang@tv365.net>
2019-01-10 18:24:16 +0800

    顶点着色器和片元着色器初始化

--- VideoPlay/practice_04_opengl/src/main/cpp/native-lib.cpp
+++ VideoPlay/practice_04_opengl/src/main/cpp/native-lib.cpp
@@ -12,12 +12,12 @@
 
 //顶点着色器glsl
 #define GET_STR(x) #x
-static const  char *vertextShader = GET_STR(
+static const  char *vertexShader = GET_STR(
         attribute vec4 aPosition; //顶点坐标
         attribute vec2 aTexCoord; //材质顶点坐标
-        varying vex2 vTexCoord;  //输出的材质坐标
+        varying vec2 vTexCoord;  //输出的材质坐标
         void main(){
-            vTExCoord = vec2(aTexCoord.x, 1.0-aTexCoord.y);
+            vTexCoord = vec2(aTexCoord.x, 1.0-aTexCoord.y);
             gl_Position = aPosition;
         }
 
@@ -25,15 +25,24 @@ static const  char *vertextShader = GET_STR(
 
 //片元着色器，软解码和部分x86硬解码
 static const char *fragYUV420P = GET_STR(
-    precision medium float;  //精度
-    varyint vec2 vTexCoord;  //顶点着色器传递的坐标
+    precision mediump float;  //精度
+    varying vec2 vTexCoord;  //顶点着色器传递的坐标
     uniform sampler2D yTexture; //输入的材质 (不透明度， 单像素)
     uniform sampler2D uTexture;
     uniform sampler2D vTexture;
 
     void main(){
-        vTexCoord vec2(aTexCoord.x, 1.0-aTexCoord.y);
-        gl_Position = aPosition;
+        vec3 yuv;
+        vec3 rgb;
+        yuv.r = texture2D(yTexture,vTexCoord).r;
+        yuv.g = texture2D(uTexture,vTexCoord).r- 0.5;
+        yuv.b = texture2D(vTexture,vTexCoord).r - 0.5;
+        rgb = mat3(1.0,     1.0,        1.0,
+                   0.0, -0.39465, 2.03211,
+                   1.13983, -0.58060, 0.0)*yuv;
+        //输出像素颜色
+        gl_FragColor = vec4(rgb,1.0);
+
     }
 );
 
@@ -130,7 +139,7 @@ Java_com_practice_1opengl_PlayView_open(JNIEnv *env, jobject instance, jstring u
     LOGD("EGL Init Success!");
 
     //顶点shader初始化
-    GLint vsh = InitShader(vertextShader, GL_VERTEX_SHADER);
+    GLint vsh = InitShader(vertexShader, GL_VERTEX_SHADER);
 
     //片元yuv420 shader初始化
     GLint fsh = InitShader(fragYUV420P, GL_FRAGMENT_SHADER);
