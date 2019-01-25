c3a246011e946833051024eaa3e63cce22edacae
叶亮 <yeliang@tv365.net>
2019-01-25 19:23:05 +0800

    添加cpp环境

---
+++ VideoPlay/practice_05_decode/src/cpp/native-lib.cpp
@@ -0,0 +1,15 @@
+//
+// Created by 叶亮 on 2019/1/25.
+//
+
+#include <jni.h>
+#include <string>
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_videoplay_MainActivity_decode(JNIEnv *env, jobject instance) {
+
+
+}
+
+
