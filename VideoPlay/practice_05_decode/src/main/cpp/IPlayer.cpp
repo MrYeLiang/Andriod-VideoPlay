b767667696119b4d0991b5bfa5600c6ed43f62ec
叶亮 <yeliang@aggrx.com>
2019-03-28 15:49:05 +0800

    seekbar滑动到指定位置播放(暂不能播放)

--- VideoPlay/practice_05_decode/src/main/cpp/IPlayer.cpp
+++ VideoPlay/practice_05_decode/src/main/cpp/IPlayer.cpp
@@ -188,12 +188,57 @@ void IPlayer::SetPause(bool isP)
 
 bool IPlayer::Seek(double pos)
 {
-    bool re = false;
+    bool re;
+    if(!demux) return false;
+    //暂停所有线程
+    SetPause(true);
     mux.lock();
-    if(demux)
+    //清理缓冲
+    //2 清理缓冲队列
+    if(vdecode)
+        vdecode->Clear();//清理缓冲队列，清理ffmepg的缓冲
+    if(adecode)
+        adecode->Clear();
+    if(audioPlay)
+        audioPlay->Clear();
+
+    re = demux->Seek(pos);//seek跳转到关键帧
+    if(!vdecode){
+        mux.unlock();
+        SetPause(false);
+        return re;
+    }
+    //解码到实际需要显示的帧
+    int seekPts = pos*demux->totalMs;
+    while (!isExit)
     {
-        re = demux->Seek(pos);
+        XData pkt = demux->Read();
+        if(pkt.size <= 0) break;
+        if(pkt.isAudio){
+            if(pkt.pts < seekPts){
+                pkt.Drop();
+                continue;
+            }
+
+            //写入缓冲队列
+            demux->Notify(pkt);
+            continue;
+        }
+        //解码需要显示的帧之前的数据
+        vdecode->SendPacket(pkt);
+        pkt.Drop();
+        XData data = vdecode->RecvFrame();
+        if(data.size <= 0)
+        {
+            continue;
+        }
+        if(data.pts >= seekPts)
+        {
+            break;
+        }
     }
     mux.unlock();
+
+    SetPause(false);
     return re;
 }
